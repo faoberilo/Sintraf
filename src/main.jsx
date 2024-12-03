aec1efc0f6dbd74589df3433788e6006f48f0d56
Felipe Amaral Oliveira <89050385+faoberilo@users.noreply.github.com>
2024-12-02 21:23:49 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,12 @@ import './index.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div className='container'>
-      <img className="logo" src='https://sintraftomg.com.br/admin/img/midia/thumb.noticias-sintraf.29-11-2024.16-14-34.0.png'/>
+      <img className="logo" src='https://sintraftomg.com.br/0teste/logo.png'/>
       <div className='buttons'>
 
-          <a href='https://sintraftomg.com.br/conteudo-noticias/pasta_uploads/SINTRAFTOMG-%20Previsao%20Orcamentaria.pdf' target="_blank" rel="noopener noreferrer" className="button"> Edital Plano Orçamentário 2025</a>
+          <a href='https://sintraftomg.com.br/0teste/previsao.pdf' target="_blank" rel="noopener noreferrer" className="button"> Edital Plano Orçamentário 2025</a>
 
-          <a href='' target="_blank" rel="noopener noreferrer" className="button"> Vote Aqui</a>
+          <a href='https://bancarios.votabem.com.br/' target="_blank" rel="noopener noreferrer" className="button"> Vote Aqui</a>
 
 
       </div>
